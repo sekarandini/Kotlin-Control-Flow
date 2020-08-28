2e722d75fb59ad5d7a5cc53e114a4b6908e85230
sekarandini <sekarputri.andini29@gmail.com>
2020-08-28 13:00:01 +0700

    kotlin control flow

---
+++ src/main/kotlin/Break dan Continue.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val listOfInt = listOf(1,2,3,null,5,null,7)
+    for (i in listOfInt)
+        print(i)
+}
---
+++ src/main/kotlin/When Expressions.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val value = 7
+
+    when(value){
+        6 -> println("Value is 6")
+        7 -> println("Value is 7")
+        8 -> println("Value is 8")
+    }
+}
